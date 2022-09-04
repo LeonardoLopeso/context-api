5d2e95c98349d119424ebe7d29654e18b85d545f
LeonardoLopeso <l.lopes355@gmail.com>
2022-09-03 22:07:46 -0300

    Criado rotas, criado Contexto e provider

---
+++ src/contexts/Context.tsx
@@ -0,0 +1,25 @@
+import React, { createContext } from "react";
+
+type ContextType = {
+    name: string;
+    age: number;
+}
+
+interface IReactFC {
+    children: React.ReactNode
+}
+
+const initialState = {
+    name: 'Leonardo',
+    age: 90
+}
+
+export const Context = createContext<ContextType>(initialState);
+
+export const ContextProvider = ({ children }: IReactFC) => {
+    return (
+        <Context.Provider value={initialState}>
+            {children}
+        </Context.Provider>
+    )
+}
